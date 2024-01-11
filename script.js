ff45f9a5a606b43b2a2d9ceebe0a6bef27431852
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2024-01-11 14:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+Car.prototype.getModel = function() {
+  return `Make: ${this.make}, Model: ${this.model}`;
+}
+
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype, {
+  constructor: {
+    value: SportsCar,
+    enumerable: false,
+    configurable: true,
+	writable: true
+  }
+});
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
